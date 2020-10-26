02f2fa52d9f433a68bbd4cb1c70537f70f665381
Filip Stål <filip.stal@outlook.com>
2020-10-26 23:11:25 +0100

    fetch and list blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from './components/layout'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>My Blog</h1>
-      <p>Posts will show up here later on</p>
+      <ul>
+        {data.allMarkdownRemark.edges.map((el, i) => {
+          const { title, date } = el.node.frontmatter
+          return (
+            <li key={i}>
+              <h2>{title}</h2>
+              <p>{date}</p>
+            </li>
+          )
+        })}
+      </ul>
     </Layout>
   )
 }
