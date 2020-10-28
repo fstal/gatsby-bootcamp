fdeb3ff0cc9b875e021d69fe9f08bc9508fa343b
Filip Stål <filip.stal@outlook.com>
2020-10-28 23:22:50 +0100

    add and render post data in blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-const Blog = () => {
-  return <Layout>This is a blog template</Layout>
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = ({ data }) => {
+  const { title, date } = data.markdownRemark.frontmatter
+  return (
+    <Layout>
+      <h1>{title}</h1>
+      <p> {date}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
+    </Layout>
+  )
 }
 
 export default Blog
