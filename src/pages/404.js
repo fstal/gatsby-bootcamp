220f219d518c32e55c3f505531533a537e81485d
Filip Stål <filip.stal@outlook.com>
2020-11-03 23:20:14 +0100

    change text 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <Layout>
       <Head title="404" />
-      <h1>Page not found</h1>
+      <h1>Sorry, page not found</h1>
       <p>
         <Link to="/">Head home</Link>
       </p>
